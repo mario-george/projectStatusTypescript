32327b6171b45703f81f53e7d0a4baebc3273a16
mario-george <vortexspades@gmail.com>
2023-08-27 22:22:06 +0300

    created an project status class

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,31 @@
+class ProjectState {
+  // we will make using static only one instace from this class
+  private static instance: ProjectState;
+  private constructor() {}
+
+  private projects: any[] = [];
+  addProject(title: string, description: string, people: number) {
+    const newProject = {
+      id: Math.random().toString(),
+      title,
+      description,
+      people,
+    };
+    this.projects.push(newProject);
+    return;
+    // return newProject;
+  }
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new ProjectState();
+    return this.instance;
+  }
+}
+
+const projectStateInstance = ProjectState.getInstance();
+
 interface ValidationInterface {
   value: string | number;
   required?: boolean;
@@ -12,24 +40,32 @@ function validate(validationObject: ValidationInterface) {
   let isValid = true;
   if (validationObject.required && (value == null || value == undefined)) {
     // isValid = isValid && !!value;
+    console.log(1);
     isValid = false;
   }
-  if (typeof value == "string" && validationObject.minLength != null) {
+  if (validationObject.minLength != null) {
     // if it is a number i ignore because it doesn't make sense
-    isValid = isValid && value.trim().length > validationObject.minLength;
+
+    isValid =
+      isValid && value.toString().trim().length >= validationObject.minLength;
   }
-  if (typeof value == "string" && validationObject.maxLength != null) {
+  if (validationObject.maxLength != null) {
     // if it is a number i ignore because it doesn't make sense
-    isValid = isValid && value.trim().length < validationObject.maxLength;
+
+    isValid =
+      isValid && value.toString().trim().length <= validationObject.maxLength;
   }
   // if min is 0 validationObject.min won't execute so use !=null
   if (typeof value == "number" && validationObject.min != null) {
     // if it is a number i ignore because it doesn't make sense
-    isValid = isValid && value > validationObject.min;
+    console.log(value >= validationObject.min);
+    isValid = isValid && value >= validationObject.min;
   }
   if (typeof value == "number" && validationObject.max != null) {
     // if it is a number i ignore because it doesn't make sense
-    isValid = isValid && value < validationObject.max;
+    console.log(value <= validationObject.max);
+
+    isValid = isValid && value <= validationObject.max;
   }
   return isValid;
 }
@@ -89,6 +125,8 @@ class ProjectInput {
     // here without bind this from event Listener the this will refer to the event not the object/instance
     let resultOfGathering = this.gatherUserInput();
     if (resultOfGathering) {
+      // get ul element and add the list
+
       console.log(this.titleInput.value);
       console.log(this.peopleInput.value);
       console.log(this.descriptionInput.value);
@@ -105,7 +143,25 @@ class ProjectInput {
     let t = this.titleInput.value;
     let d = this.descriptionInput.value;
     let p = +this.peopleInput.value;
-
+    const titleValidationObj: ValidationInterface = {
+      value: t,
+      required: true,
+    };
+    const descriptionValidationObj: ValidationInterface = {
+      value: d,
+      required: true,
+      minLength: 5,
+    };
+    const peopleValidationObj: ValidationInterface = {
+      value: p,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+    let titleValidation = validate(titleValidationObj);
+    let descriptionValidation = validate(descriptionValidationObj);
+    let peopleValidation = validate(peopleValidationObj);
+    /* // old validation
     if (
       t.trim().length === 0 ||
       d.trim().length === 0 ||
@@ -115,6 +171,14 @@ class ProjectInput {
       return;
     }
     return [t, d, p];
+   */
+
+    if (!titleValidation || !descriptionValidation || !peopleValidation) {
+      alert("Invalid user input please try again");
+      return;
+    } else {
+      return [t, d, p];
+    }
   }
   private clearInputFields() {
     this.titleInput.value = "";
@@ -122,4 +186,35 @@ class ProjectInput {
     this.peopleInput.value = "";
   }
 }
+
+class ProjectList {
+  element: HTMLElement;
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+
+  constructor(private type: "active" | "finished") {
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    // get the first import node
+    let importedNode = document.importNode(this.templateElement.content, true);
+    // section eleement
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderItems();
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+  private renderItems() {
+    let id = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = id;
+    let h2 = this.element.querySelector("h2")!;
+    h2.textContent = `${this.type.toUpperCase()} Projects`;
+  }
+}
 const p = new ProjectInput();
+const active = new ProjectList("active");
+const finished = new ProjectList("finished");
