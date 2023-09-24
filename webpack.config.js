d88b1376b38e816a15f5ac2857b1b7c55f787cc0
mario-george <vortexspades@gmail.com>
2023-09-24 15:29:14 +0300

    webpack config added

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,28 @@
 // npm i --save-dev typescript ts-loader webpack webpack-dev-server webpack-cli
 // webpack config has a node js export
-
+const path = require("path");
 module.exports = {
+  entry: "./src/app.ts",
 
-    entry:'./src/app.ts'
+  output: {
+    filename: "bundle.js",
+    // needs an absolute path
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /.ts$/,
+        // use package ts-loader to load ts extention it will use the tsconfig by default
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+//  it generates a source map and includes it inline within the generated JavaScript bundle
+  devtool:'inline-source-map',
+  resolve: {
+    // we will tell the webpack which extenstions it will add to the end of the imports
+    extensions: [".ts", ".js"],
+  },
 };
